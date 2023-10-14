4039957bb158c28cd591319a3a589990143776e1
KakarlaSaiKrupa <141944236+KakarlaSaiKrupa@users.noreply.github.com>
2023-10-14 20:27:37 +0530

    Update script.js

--- TXON_02/script.js
+++ TXON_02/script.js
@@ -1,1 +1,33 @@
+let resultElement = document.getElementById('result');
+let currentExpression = '';
 
+function appendNumber(number) {
+    currentExpression += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    currentExpression += operator;
+    updateDisplay();
+}
+
+function clearScreen() {
+    currentExpression = '';
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        let result = eval(currentExpression);
+        currentExpression = result.toString();
+        updateDisplay();
+    } catch (error) {
+        currentExpression = '';
+        updateDisplay();
+        alert('Invalid input!');
+    }
+}
+
+function updateDisplay() {
+    resultElement.value = currentExpression;
+}
